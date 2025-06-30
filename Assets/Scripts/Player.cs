bd333ca1a46eb939b362c334f04f828f7194ff5c
KERVAL4 <vazha.kervalidze@gmail.com>
2025-06-30 06:55:08 -0700

    end

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -131,4 +131,17 @@ public class Player : MonoBehaviour
             }
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Objective"))
+        {
+            enabled = false;
+            GameManager.Instance.LevelComplete();
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            enabled = false;
+            GameManager.Instance.LevelFailed();
+        }
+    }
 }
